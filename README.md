f58ba4894db5a861386a03a55400a7dd17e43b82
Adrianna Gerek <aduzja@gmail.com>
2025-01-08 12:26:05 +0100

    Initialize project

---
+++ README.md
@@ -0,0 +1,3 @@
+# explore
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:explore/src/application/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
