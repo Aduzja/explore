4b869a3973e0f709a46c2f705cb27b9f5ef09a18
Adrianna Gerek <aduzja@gmail.com>
2025-01-09 14:37:32 +0100

    Add specific colors

--- lib/src/application/constants/app_colors.dart
+++ lib/src/application/constants/app_colors.dart
@@ -3,4 +3,9 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
+  static const Color lightGrey = Color(0xFFE4ECED);
+  static const Color darkGrey = Color(0xFF4D4C4C);
+  static const Color backgroundGrey = Color(0xFFEFEFEF);
+  static const Color blue = Color(0xFF0794D3);
+  static const Color green = Color(0xFF0DDEAE);
 }
